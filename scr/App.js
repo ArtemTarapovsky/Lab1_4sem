4457f71782f05d2f354263fdc086c4f0e694cad6
ArtemTarapovsky <161740158+ArtemTarapovsky@users.noreply.github.com>
2025-05-25 15:39:23 +0700

    Update App.js

--- scr/App.js
+++ scr/App.js
@@ -1,1 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Form from './pages/Form';
 
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/add" element={<Form />} />
+      <Route path="/edit/:id" element={<Form />} />
+    </Routes>
+  );
+}
+
+export default App;
